f2c6aa34e05fbce00adede1f7da78c6dc51009ad
wanuluu <jorgemedinaleigh@protonmail.com>
2020-12-25 21:44:54 -0300

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody body;
+    float horizontal;
+    float vertical;
+    public float runSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector3(horizontal * runSpeed, 0, vertical * runSpeed);
+    }
+}
